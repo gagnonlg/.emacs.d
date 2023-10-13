587bd0f2b531f67e406d3ed01006d2c5de30f628
Louis-Guillaume Gagnon <gagnonlg@protonmail.com>
2023-10-13 09:54:49 +0200

    Initial commit

---
+++ bootstrap.el
@@ -0,0 +1,6 @@
+(require 'org)
+(let ((vc-follow-symlinks nil))
+  (if (file-newer-than-file-p "~/.emacs.d/init.org" "~/.emacs.d/init.el")
+      (org-babel-tangle-file "~/.emacs.d/init.org")))
+(load-file "~/.emacs.d/init.el")
+(put 'downcase-region 'disabled nil)
